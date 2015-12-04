dcc7388e594fba62e9d687bd445e9538de0c7a7d
railsbeginner <bartosz.dyja@gmail.com>
2015-12-04 17:55:55 +0100

    Configure Account associations

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,8 @@ class Student < ActiveRecord::Base
   has_many :subject_item_notes, dependent: :destroy
   has_many :subject_items, through: :participations
 
+  has_one :account, dependent: :destroy
+
   accepts_nested_attributes_for :subject_items 
 
   validates :first_name, :last_name, presence: true
